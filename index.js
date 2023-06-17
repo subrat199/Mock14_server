2a6d316b65fffe743d53068cdbeaa2a00e0e2be3
subrat199 <subratt01998@gmail.com>
2023-06-17 11:38:25 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors=require("cors");
+const { connection } = require("./db");
+const { userRouter } = require("./routes/User.routes");
+const {auth}=require("./Middlewere/authMiddleware");
+const jwt = require("jsonwebtoken");
+// const { userProfile } = require("./model/userProfile");
+const app = express();
+app.use(express.json());
+app.use(cors())
+app.use("/users", userRouter);
+app.get("/", (req, res) => {
+    res.send("welcome Home page");
+  });
+app.use(auth)
+// app.use("/profile",userProfileRouter)
+app.listen(8080, async () => {
+  try {
+    await connection;
+    console.log("connected");
+  } catch (error) {
+    console.log("Not coonect to db");
+    console.log(error);
+  }
+  console.log("Server listening on 8080");
+});
---
+++ model/userModel.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const userSchema=mongoose.Schema({
+    email:{type:String,require:true},
+    password:{type:String,require:true},
+   
+},{
+    versionKey:false
+})
+const userModel=mongoose.model('User',userSchema)
+module.exports={
+    userModel
+}
