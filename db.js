081b8543fb8af1c2c044e1646628472d782e3d29
subrat199 <subratt01998@gmail.com>
2023-06-17 11:42:31 +0530

    deploy

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const connection=mongoose.connect("mongodb+srv://subrat:nayak@cluster0.ccovhmt.mongodb.net/Fitness?retryWrites=true&w=majority")
+const connection=mongoose.connect("mongodb+srv://subrat:nayak@cluster0.ccovhmt.mongodb.net/Quiz?retryWrites=true&w=majority")
 module.exports={
     connection
 }
