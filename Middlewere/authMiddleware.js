680cf18b029721bcf9fdf73c01a4c5aff5c0162c
subrat199 <subratt01998@gmail.com>
2023-06-17 14:44:18 +0530

    deploy

--- Middlewere/authMiddleware.js
+++ Middlewere/authMiddleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const auth = (req,res,next)=>{
-    const  token  = req.headers.authorization;
+    const  token  = localStorage.getItem("token");
    
     if(token){
         try {
